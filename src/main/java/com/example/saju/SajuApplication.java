5d04d4dbf61ea475e00d7b4fd591cd06dd40a315
wlsql852@naver.com <wlsql852@naver.com>
2025-12-30 20:52:38 +0900

    commit init

---
+++ src/main/java/com/example/saju/SajuApplication.java
@@ -0,0 +1,13 @@
+package com.example.saju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SajuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/saju/SajuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SajuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
